c1e9d4c4ea23fb2512fe2c9283bf5b51f4854af8
Joe Bullard <joekbullard@gmail.com>
2022-10-12 08:22:31 +0100

    rework of index.html

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['Poppins', 'sans-serif'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],   
+    },
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '20px',
+      '12': '12px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
