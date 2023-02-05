3e395a3cb7aece465a08e40bebaac2bb12625e9e
Joe Bullard <joekbullard@gmail.com>
2023-02-05 13:17:32 +0000

    conver to jekyll

---
+++ _site/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['Poppins', 'sans-serif'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],   
+    },
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '20px',
+      '12': '12px',
+    },
+    extend: {
+      spacing: {
+        '128': '400px',
+      },
+    },
+  },
+  plugins: [],
+}
